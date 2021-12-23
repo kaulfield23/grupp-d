e7f000dd8d662a51c68dc3edde99bce3228ed73d
Pontus Vallgren <84342285+PontusVallgren@users.noreply.github.com>
2021-12-23 15:31:07 +0100

    Change to english
    Opsie

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -3,7 +3,7 @@ import { loadMoviePage } from "./pages/moviepage";
 export const checking = () => {
   console.log("navbar.js is loaded");
 };
-const filmerBtn = document.querySelector(".menuMovies");
-filmerBtn.addEventListener("click", () => {
+const movieBtn = document.querySelector(".menuMovies");
+movieBtn.addEventListener("click", () => {
   loadMoviePage();
 });
