519b0a3bff63d4e396ff12a09ab457da48540b43
JohanEkstroem <90635692+JohanEkstroem@users.noreply.github.com>
2021-12-23 16:44:11 +0100

    Added resonsive navbar

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,9 +1,21 @@
 import { loadMoviePage } from "./pages/moviepage";
 
 export const checking = () => {
-  console.log("navbar.js is loaded");
+    console.log("navbar.js is loaded");
 };
 const movieBtn = document.querySelector(".menuMovies");
 movieBtn.addEventListener("click", () => {
-  loadMoviePage();
+    loadMoviePage();
 });
+
+export const navbarResponsive = () => {
+    const hamburger = document.querySelector('.hamburger');
+    hamburger.addEventListener('click', () => {
+        let navbar = document.querySelector('#navbar');
+        if (navbar.className === "topnav") {
+            navbar.className += " responsive";
+        } else {
+            navbar.className = "topnav"
+        }
+    })
+}
