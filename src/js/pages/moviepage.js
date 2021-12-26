1060ab334b40d46c3c823e6e114fac7180c257d3
PontusVallgren <pontus.wallgren92@gmail.com>
2021-12-26 21:42:28 +0100

    Load moviePage with today movies as default setting

--- src/js/pages/moviepage.js
+++ src/js/pages/moviepage.js
@@ -1,7 +1,12 @@
 import { renderMovies } from "../render.js";
 import { loadMovieAPI } from "../api.js";
+import { moviePageMain } from "../movieMain.js";
+import { filterToday } from "../filter.js";
 
 export const loadMoviePage = async () => {
   const data = await loadMovieAPI();
-  renderMovies(data);
+  const container = document.querySelector(".movies");
+  container.innerHTML = "";
+  moviePageMain();
+  renderMovies(filterToday(data));
 };
