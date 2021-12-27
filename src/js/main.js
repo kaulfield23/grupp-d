c4b77894b23cde50b53e95296e4558aa6b60eb90
PontusVallgren <pontus.wallgren92@gmail.com>
2021-12-27 14:34:21 +0100

    created IIFE function for loading functions onload

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,8 @@ import { carousel } from "./carousel";
 import { renderCinemaCommercial } from "./commercial";
 import { navbarResponsive } from "./navbar";
 
-window.onload = function () {
+(() => {
   carousel();
   navbarResponsive();
   renderCinemaCommercial();
-};
+})();
