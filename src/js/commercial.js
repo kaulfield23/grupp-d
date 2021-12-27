0aa09578c17af84a333d4f46fe7efd0ceec429d2
kaulfield23 <kaulfield23@gmail.com>
2021-12-27 09:36:43 +0100

    added commercial and fixed logo problem

--- src/js/commercial.js
+++ src/js/commercial.js
@@ -1,15 +1,30 @@
 export const renderCinemaCommercial = () => {
   console.log("cinema commercial loaded");
-  const eventContainer = document.querySelector(".cinemaEvent");
+  //description for events
+  const cEventDescription = [
+    "Come and check out the christmas theme of our cinema!(will change all texts to swedish later)",
+    "share the link of this event and get discount until this year!",
+    "For the customers who wear a mask, we offer 20% discount!",
+  ];
   for (let i = 1; i <= 3; i++) {
-    const cinemaEvents = createElement("div", "cEvents");
+    const eventContainer = document.querySelector(".cinemaEvent");
+    //create div, img and span in cinemaEvent
+    const cinemaEvents = createElement("div", "cEvents right");
     const eventImg = createElement("img", "cImg");
     eventImg.src = `src/img/cinema${i}.jpg`;
+    const emptyDiv = createElement("div", "emptyDiv");
+    const eventDesc = createElement("span", "cEventDesc");
+    //give description
+    eventDesc.innerText = cEventDescription[i - 1];
+    //append
     cinemaEvents.appendChild(eventImg);
+    cinemaEvents.appendChild(emptyDiv);
+    emptyDiv.appendChild(eventDesc);
     eventContainer.appendChild(cinemaEvents);
   }
 };
 
+//function to create element
 function createElement(type, name) {
   const element = document.createElement(type);
   element.setAttribute("class", name);
