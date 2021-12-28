2d80e2c4eae7ad15f8fa6ec246ef58286bdc11ad
Roger Hagerbaum <roger.hagerbaum@lerniastudent.se>
2021-12-29 00:52:56 +0100

    added styling

--- src/js/pages/covidInfo.js
+++ src/js/pages/covidInfo.js
@@ -1,6 +1,6 @@
 const template = document.createElement('template')
 template.innerHTML = `
-<h1>Ett tryggt biobesök</h1>
+<div id="covidInformation"><h1>Ett tryggt biobesök</h1>
 
 <p>Från och med den 23 december inför regeringen nya smittskyddsåtgärder. Då behöver du som har fyllt 18 år kunna visa upp ett giltigt vaccinationsbevis för Covid-19 och ID-handling med foto när du besöker oss. 
 Det gäller samtliga våra biografer och salonger.
@@ -19,11 +19,13 @@ Om du av medicinska skäl inte bör ta vaccinet, eller endast får ta en dos, be
 <h3>Bra att tänka på</h3>
 <p>Har du sedan tidigare laddat ner ett vaccinationsbevis är det en bra idé att kontrollera att det fortfarande är giltigt till ditt besök hos oss. Du kan enkelt förnya ditt bevis på covidbevis.se
 Observera att så kallade testbevis, negativt test och tillfrisknandebevis inte räknas som vaccinationsbevis och kan ej användas på biografen.
-Kom gärna i god tid. Ibland kan det bli kö när kontroll av vaccinationsbevis sker i entrén till biografen.</p>
+Kom gärna i god tid. Ibland kan det bli kö när kontroll av vaccinationsbevis sker i entrén till biografen.</p></div>
 `
 
 export const covidInforation = () => {
     const container = document.querySelector(".movies");
     container.innerHTML = "";
     container.appendChild(template.content.cloneNode(true));
+  
+    
   };
