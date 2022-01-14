f66bd6c8deb63600e38976d077c50a1f08f2806b
kaulfield23 <kaulfield23@gmail.com>
2022-01-14 17:57:02 +0100

    SSR with express-handlebars

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
-import express, { response } from "express";
 import fs from "fs/promises";
+import { engine } from "express-handlebars";
+import express from "express";
 
 const app = express();
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./templates");
 
 app.get("/", async (req, res) => {
-  try {
-    const fileBuf = await fs.readFile("./index.html", (err, data) => {
-      console.info(data);
-      console.info(err);
-    });
-    res.type("html");
-    res.send(fileBuf);
-} catch (err) {
-    console.log(err);
-    res.status(404);
-    res.send("page not found");
-}
+    res.render('index', {layout : 'main'});
 });
 
-// app.use(express.static('./'));
+app.get("/moviepage", async (req, res) => {
+  res.render('moviePage', {layout : 'main'});
 
-app.get('/*', async (req, res) =>{
-    const fileName = req.path;
-    const fileBuf = await fs.readFile(`./${fileName}`);
-    const type = fileName.split('.')[1];
-    res.type(type);
-    res.send(fileBuf);
-})
+});
+app.use(express.static("./"));
+
+// app.get('/*', async (req, res) =>{
+//     const fileName = req.path;
+//     const fileBuf = await fs.readFile(`./${fileName}`);
+//     const type = fileName.split('.')[1];
+//     res.type(type);
+//     res.send(fileBuf);
+// })
 
 app.listen(5080);
